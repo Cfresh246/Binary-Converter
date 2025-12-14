61c00a9c83d1464957a09dfd4941877a8b4ffd6c
Chris Anthony Lafleur <chris.lafleur246@gmail.com>
2025-12-13 20:19:18 -0500

    encoding added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Binary_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("110₂");
+
+        }
+    }
+}
