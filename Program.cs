f77745a21bddb65dee7984ab68d328c58d09860a
Chris Anthony Lafleur <chris.lafleur246@gmail.com>
2025-12-22 13:47:10 -0500

    binary number to decimal

--- Program.cs
+++ Program.cs
@@ -7,9 +7,25 @@ namespace Binary_Converter
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("110₂");
 
+            Console.Write("Enter a number: ");
+            string? number = Console.ReadLine().Replace(" ", "");
+
+            BinaryToDecimal(number);
+        }
+        static void BinaryToDecimal(string n)
+        {
+            double total = 0;
+            int index = n.Length - 1;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                
+                total += int.Parse(n[i].ToString()) * Math.Pow(2, index);
+                index--;
+            }
+            Console.WriteLine(total);
         }
     }
 }
+ 
