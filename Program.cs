ae2ef4e23024d73c97e1f10b1b62071cca536eff
Chris Anthony Lafleur <chris.lafleur246@gmail.com>
2025-12-23 20:23:43 -0500

    added read binary helper

--- Program.cs
+++ Program.cs
@@ -8,23 +8,46 @@ namespace Binary_Converter
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            Console.Write("Enter a number: ");
-            string? number = Console.ReadLine().Replace(" ", "");
-
-            BinaryToDecimal(number);
+            BinaryToDecimal();
         }
-        static void BinaryToDecimal(string n)
+        static void BinaryToDecimal()
         {
-            double total = 0;
-            int index = n.Length - 1;
+            string number = ReadBinary();
 
-            for (int i = 0; i < n.Length; i++)
+            double total = 0;
+            int index = number.Length - 1;
+
+            for (int i = 0; i < number.Length; i++)
             {
                 
-                total += int.Parse(n[i].ToString()) * Math.Pow(2, index);
+                total += int.Parse(number[i].ToString()) * Math.Pow(2, index);
                 index--;
             }
             Console.WriteLine(total);
+        }
+        static string ReadBinary()
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string? number = Console.ReadLine().Replace(" ", "");
+
+                bool IsValid = true;
+
+                foreach(char c in number)
+                {
+                    if (!char.IsDigit(c) || (c != '0' && c != '1'))
+                    {
+                        IsValid = false; 
+                    }
+                }
+
+                if (IsValid)
+                {
+                    return number;
+                }
+            }
+
         }
     }
 }
